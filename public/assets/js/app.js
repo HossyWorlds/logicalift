d2054d34ace1e3470d37aa578912cd4e7b277d6f
HossyWorlds <sakkashu@outlook.jp>
2023-11-12 19:17:04 +0900

    もっとみる

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,11 +6,18 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-// window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery');
+window.addEventListener('DOMContentLoaded', function(){
+    // 初期表示数
+    const init = 5;
+    //追加表示数
+    const more = 3;
+    //初期表示数以降のリスト内容を非表示に
+    
+    $("menusList li:nth-child(n+" + (init+1) + ")").hide();
+    
+    //つづきは「https://mgmgblog.com/post-3374/」のリンク
+    /** jQueryの処理 */ 
 
-// 初期表示数
-const init = 5;
-//追加表示数
-const more = 3;
+});
 
-//つづきは「https://mgmgblog.com/post-3374/」のリンク
